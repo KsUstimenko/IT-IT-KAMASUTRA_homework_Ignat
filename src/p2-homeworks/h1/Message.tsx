df026eb8c7106260519247ac38b3e6adadf8b6d0
kseniia <ksusha.mamatova98@gmail.com>
2022-08-24 09:47:35 +0300

    refactoring

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './styles.css'
+import  s from './message.module.css'
 
 type messageType = {
     avatar: string
@@ -11,11 +11,12 @@ type messageType = {
 
 function Message(props: messageType) {
     return (
-        <div className="container">
-            <img src={props.avatar} alt="Avatar"/>
-            <span>{props.name}</span>
-            <p>{props.message}</p>
-            <span className="time-right">{props.time}</span>
+        <div className={s.container}>
+            <img src={props.avatar} alt="Avatar" className={s.avatar}/>
+
+            <div className={s.name}>{props.name}</div>
+            <div className={s.message}>{props.message}</div>
+            <div className={s.time}>{props.time}</div>
         </div>
     )
 }
